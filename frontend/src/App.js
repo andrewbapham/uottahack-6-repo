4fb1e949d710fb16fe4b05547b9550cf0076a729
Andrew Pham <andrewbapham@outlook.com>
2024-03-02 09:46:42 -0500

    Add auth checking

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 import Auth0ProviderWithHistory from "./Components/Auth0Provider";
+import LoginButton from "./Components/LoginButton/LoginButton";
 import Register from "./Components/Register/Register";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { user, isLoading } = useAuth0();
   return (
-    <Auth0ProviderWithHistory>
+    <>
       <div className="App">
         <Register />
       </div>
-    </Auth0ProviderWithHistory>
+      {user ? <div>Logged in</div> : <LoginButton />}
+    </>
   );
 }
 
