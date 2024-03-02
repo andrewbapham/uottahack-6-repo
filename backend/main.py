d170ae89f954f8cd5eed3c43fe5506333fe2c5fe
Andrew Pham <andrewbapham@outlook.com>
2024-03-02 02:28:12 -0500

    API naming changes

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -8,23 +9,23 @@ async def root():
     return {"message": "Hello World"}
 
 # USER API
-@app.get("/users/{user_id}")
+@app.get("/users/user/{user_id}")
 async def get_user_by_id(user_id: int):
     return {"user_id": user_id}
 
-@app.post("/users/{user_id}")
+@app.post("/users/user/{user_id}")
 async def create_user(user_id: int):
     return {"user_id": user_id}
 
-@app.put("/users/{user_id}")
+@app.put("/users/user/{user_id}")
 async def update_user(user_id: int):
     return {"user_id": user_id}
 
-@app.delete("/users/{user_id}")
+@app.delete("/users/user/{user_id}")
 async def delete_user(user_id: int):
     return {"user_id": user_id}
 
 #Get users using a criteria object in the request body
-@app.get("/users")
+@app.get("/users/users")
 async def get_users(criteria: dict):
     return {"criteria": criteria}
