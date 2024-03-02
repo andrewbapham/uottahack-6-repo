ed4c8ff11ad4a5f2649abc3c3cc925207fd1e088
lamseamus <106627989+lamseamus@users.noreply.github.com>
2024-03-02 11:17:32 -0500

    Add files via upload

---
+++ backend/ComputeSimilarityMatrix.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np 
+import scipy
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+
+
+def clean_df(df):
+    """
+    Clean the DataFrame by encoding object columns and standardizing numerical columns.
+    """
+    # Encode Object Columns
+    encoded_object_columns = pd.get_dummies(df.select_dtypes(include=['object']))
+
+    # Standardize Numerical Columns
+    numerical_columns = df.select_dtypes(include=['int64', 'float64'])
+    scaler = StandardScaler()
+    scaled_numerical_columns = pd.DataFrame(scaler.fit_transform(numerical_columns), columns=numerical_columns.columns)
+  
+    features_matrix = pd.concat([encoded_object_columns, scaled_numerical_columns], axis=1)
+    
+    return features_matrix
+
+def compute_similarity_matrix(features_matrix):
+    """
+    Reduce dimensionality and compute similarity matrix 
+    """
+    
+    pca = PCA(n_components=0.95) 
+    features_matrix_reduced = pca.fit_transform(features_matrix)
+    similarity_matrix = cosine_similarity(features_matrix_reduced)
+
+    return similarity_matrix
+
+
+
+def get_similarity_matrix(df, similarity_matrix, user_id):
+    """
+    Get the top 10 most similar user ids based on a similarity matrix.
+    """
+    # Get index of the user_id in the dataset
+    user_index = next(index for index, profile in enumerate(df) if df["user_id"] == user_id)
+
+    # Get similarity values for the specified user and sort them
+    similar_scores = list(enumerate(similarity_matrix[user_index]))
+    similar_scores = sorted(similar_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the top 10 most similar user indices, excluding the user themselves
+    top_10_similar_indices = [i for i, score in similar_scores[1:11]]
+
+    # Return the user_ids of the top 10 similar profiles
+    return [profiles[i]["user_id"] for i in top_10_similar_indices]
